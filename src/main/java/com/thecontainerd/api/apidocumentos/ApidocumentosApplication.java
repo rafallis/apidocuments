aa0362cb27c8d2249dff2dbeaf90599a0b69fbdc
Rafael Jardim <rafael.jardim@camara.leg.br>
2022-02-16 13:36:30 -0300

    First commit

---
+++ src/main/java/com/thecontainerd/api/apidocumentos/ApidocumentosApplication.java
@@ -0,0 +1,13 @@
+package com.thecontainerd.api.apidocumentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidocumentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApidocumentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thecontainerd/api/apidocumentos/ApidocumentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecontainerd.api.apidocumentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidocumentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
