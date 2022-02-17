9ffe29eaeac0edd55b9fd22946df4ca25697d2d3
Rafael Jardim <rafael.jardim@camara.leg.br>
2022-02-17 19:51:46 -0300

    Added global CORS options

--- src/main/java/com/thecontainerd/api/apidocuments/ApidocumentosApplication.java
+++ src/main/java/com/thecontainerd/api/apidocuments/ApidocumentosApplication.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableRabbit
@@ -23,6 +26,18 @@ public class ApidocumentosApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		storageService.deleteAll();
 		storageService.init();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+			}
+		};
 	}
 
 }
